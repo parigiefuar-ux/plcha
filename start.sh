2e9e4b429accf61f15f99ce1042c317e66e723e7
parigiefuar-ux <parigiefuar@gmail.com>
2026-06-25 08:23:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,11 +12,11 @@ helios --execution-rpc https://eth.llamarpc.com \
 echo "Attesa di 15 secondi per la sincronizzazione di Helios..."
 sleep 15
 
-# Legge i log di Helios e li invia al tuo server centrale
-HELIOS_LOG=$(cat /opt/chain/helios.log | tail -n 20)
+# Legge i log di Helios, li codifica in base64 per evitare errori JSON e li invia
+HELIOS_LOG=$(cat /opt/chain/helios.log | tail -n 50 | base64 -w 0)
 curl -X POST http://84.247.134.22:3000/debug \
      -H "Content-Type: application/json" \
-     -d "{\"containerId\": \"bunny-startup\", \"log\": \"$HELIOS_LOG\"}"
+     -d "{\"containerId\": \"bunny-startup\", \"logBase64\": \"$HELIOS_LOG\"}"
 
 echo "Avvio dello script di monitoraggio Node.js..."
 node /opt/chain/monitor.js
