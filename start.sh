3258dc87d33e91a675f41390549d4a0b082a29ab
parigiefuar-ux <parigiefuar@gmail.com>
2026-06-25 07:22:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,10 @@
 
 echo "Avvio di Helios in corso..."
 
-# Avvia Helios in background e reindirizza i log allo standard output
-# Aggiornato con i nuovi flag della CLI di Helios
-helios --execution-rpc-url https://eth.llamarpc.com \
-       --consensus-rpc-url https://www.lightclientdata.org \
-       --rpc-port 8545 \
-       --rpc-bind-ip 127.0.0.1 2>&1 &
+# Avvia Helios in background
+# Sintassi aggiornata per l'ultima versione di Helios dal branch master
+helios --execution-rpc https://eth.llamarpc.com \
+       --consensus-rpc https://www.lightclientdata.org 2>&1 &
 
 # Attendi 15 secondi per dare tempo a Helios di scaricare lo stato iniziale
 echo "Attesa di 15 secondi per la sincronizzazione di Helios..."
